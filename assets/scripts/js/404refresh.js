ccdf820f5ad501ac0bc07608764d3edc054e9d4a
Yurigabriel6 <comunicacao@iigdimbarie.com.br>
2025-09-08 21:46:42 -0300

    base (404 = index)

--- assets/scripts/js/404refresh.js
+++ assets/scripts/js/404refresh.js
@@ -1,1 +1,8 @@
-window.window.location.href = "404.html";
+fetch('/404.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('conteudo').innerHTML = html;
+    })
+    .catch(() => {
+        document.getElementById('conteudo').innerHTML = '<h1>Erro ao carregar a página</h1>';
+    });
